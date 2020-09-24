e9461a0d92272fef6a4c77450d40b9fec04c768a
jsolarz <jsolarz@google.com>
2020-09-24 14:35:46 +0200

    Add patching of ingress to create-server.sh

--- server/mysql-server/create-server.sh
+++ server/mysql-server/create-server.sh
@@ -2,6 +2,8 @@ export CERTS_ROOT="../../mysql-certs"
 
 kubectl delete --ignore-not-found=true -n istio-system secret mysql-credential
 
+kubectl -n istio-system patch --type=json svc istio-ingressgateway -p "$(cat gateway-patch.json)"
+
 kubectl create -n istio-system secret generic mysql-credential \
 --from-file=tls.key=$CERTS_ROOT/3_application/private/mysql-mutual-tls.jeremysolarz.app.key.pem \
 --from-file=tls.crt=$CERTS_ROOT/3_application/certs/mysql-mutual-tls.jeremysolarz.app.cert.pem \
