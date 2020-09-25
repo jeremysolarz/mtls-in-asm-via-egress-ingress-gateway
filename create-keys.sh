fef9160e6cfc58007069073efb844a4498f14696
jsolarz <jsolarz@google.com>
2020-09-26 01:56:41 +0200

    Add create-keys.sh command

---
+++ create-keys.sh
@@ -0,0 +1,12 @@
+INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+rm -rf certs
+mkdir certs
+cd mtls-go-example
+# delete old certs
+rm -rf 1_root 2_intermediate 3_application 4_client
+yes | ./generate.sh $INGRESS_HOST.nip.io mysupersecurepassword
+mv 1_root ../certs
+mv 2_intermediate ../certs
+mv 3_application ../certs
+mv 4_client ../certs
