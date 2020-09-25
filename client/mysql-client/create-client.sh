2818c36e2d55497eb1f6ccee626548216adcc47a
jsolarz <jsolarz@google.com>
2020-09-25 12:03:05 +0200

    Rename URL to SERVICE_URL to have consistency

--- client/mysql-client/create-client.sh
+++ client/mysql-client/create-client.sh
@@ -1,6 +1,6 @@
 ####
 CERTS_ROOT="../../httpbin-certs"
-URL="httpbin-mutual-tls.jeremysolarz.app"
+SERVICE_URL="httpbin-mutual-tls.jeremysolarz.app"
 
 gcloud config set project vch-anthos-demo
 gcloud container clusters get-credentials anthos-gcp --region europe-west4 --project vch-anthos-demo
@@ -8,14 +8,15 @@ gcloud container clusters get-credentials anthos-gcp --region europe-west4 --pro
 # ./clean-up.sh
 
 kubectl create -n istio-system secret tls httpbin-client-certs \
-  --key $CERTS_ROOT/4_client/private/${URL}.key.pem \
-  --cert $CERTS_ROOT/4_client/certs/${URL}.cert.pem
+  --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
+  --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem
 
 kubectl create -n istio-system secret generic httpbin-ca-certs --from-file=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
 
 kubectl -n istio-system patch --type=json deploy istio-egressgateway -p "$(cat gateway-patch.json)"
 
 sleep 15
-kubectl exec -it -n istio-system $(kubectl -n istio-system get pods -l istio=egressgateway -o jsonpath='{.items[0].metadata.name}') -- ls -al /etc/istio/httpbin-client-certs /etc/istio/httpbin-ca-certs
+kubectl exec -it -n istio-system $(kubectl -n istio-system get pods -l istio=egressgateway -o jsonpath='{.items[0].metadata.name}') \
+  -- ls -al /etc/istio/httpbin-client-certs /etc/istio/httpbin-ca-certs
 
 kubectl apply -f se-mysql.yaml
