428c285667f7d1ba3ea6eb2f1a63bd5714d59f24
jsolarz <jsolarz@google.com>
2020-09-26 00:39:39 +0200

    Extract variables from create-server.sh

--- server/httpbin-server/create-server.sh
+++ server/httpbin-server/create-server.sh
@@ -1,15 +1,19 @@
-export CERTS_ROOT="../../httpbin-certs"
 
 gcloud config set project jsolarz-sandbox
 gcloud container clusters get-credentials anthos-on-gcp --zone us-central1-c --project jsolarz-sandbox
 
-kubectl delete --ignore-not-found=true gateway mygateway
-kubectl delete --ignore-not-found=true virtualservice httpbin
-kubectl delete --ignore-not-found=true -n istio-system secret httpbin-credential httpbin-credential-cacert
+./clean-up.sh
+
+# folder where certificates are stored created by mtls-go-example
+CERTS_ROOT="../../httpbin-certs"
+
+# url of the external service
+SERVICE_URL="httpbin-mutual-tls.jeremysolarz.app"
+
 
 kubectl create -n istio-system secret generic httpbin-credential \
---from-file=tls.key=$CERTS_ROOT/3_application/private/httpbin-mutual-tls.jeremysolarz.app.key.pem \
---from-file=tls.crt=$CERTS_ROOT/3_application/certs/httpbin-mutual-tls.jeremysolarz.app.cert.pem \
+--from-file=tls.key=$CERTS_ROOT/3_application/private/${SERVICE_URL}.key.pem \
+--from-file=tls.crt=$CERTS_ROOT/3_application/certs/${SERVICE_URL}.cert.pem \
 --from-file=ca.crt=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
 
 kubectl create -n istio-system secret generic httpbin-credential-cacert \
@@ -20,15 +24,15 @@ kubectl apply -f httpbin-vs.yaml
 
 sleep 5
 # jsolarz-sanbox
-curl -v -HHost:httpbin-mutual-tls.jeremysolarz.app --resolve \
-  "httpbin-mutual-tls.jeremysolarz.app:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
+curl -v -HHost:${SERVICE_URL} --resolve \
+  "${SERVICE_URL}:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
   --cacert $CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem \
-  --cert $CERTS_ROOT/4_client/certs/httpbin-mutual-tls.jeremysolarz.app.cert.pem \
-  --key $CERTS_ROOT/4_client/private/httpbin-mutual-tls.jeremysolarz.app.key.pem \
-  "https://httpbin-mutual-tls.jeremysolarz.app:$SECURE_INGRESS_PORT/status/418"
+  --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem \
+  --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
+  "https://${SERVICE_URL}:$SECURE_INGRESS_PORT/status/418"
 
 # external
 curl -v --cacert $CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem \
-  --cert $CERTS_ROOT/4_client/certs/httpbin-mutual-tls.jeremysolarz.app.cert.pem \
-  --key $CERTS_ROOT/4_client/private/httpbin-mutual-tls.jeremysolarz.app.key.pem \
-  "https://httpbin-mutual-tls.jeremysolarz.app/status/418"
+  --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem \
+  --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
+  "https://${SERVICE_URL}/status/418"
