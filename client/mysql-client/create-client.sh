a2f1960e78d8c2df414f3b9e14829b0855019069
jsolarz <jsolarz@google.com>
2020-09-26 02:04:43 +0200

    Use setter script

--- client/mysql-client/create-client.sh
+++ client/mysql-client/create-client.sh
@@ -1,6 +1,4 @@
-gcloud config set project vch-anthos-demo
-
-gcloud container clusters get-credentials anthos-gcp --region europe-west4 --project vch-anthos-demo
+../set-project-and-cluster-client.sh
 
 ./clean-up.sh
 
