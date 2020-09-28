c865d2c951a82c6f8d7337f70c2304708ee67402
Valentin Hamburger <vhamburger@google.com>
2020-09-28 13:06:35 +0200

    minor changes

--- server/set-project-and-cluster-server.sh
+++ server/set-project-and-cluster-server.sh
@@ -1,3 +1,8 @@
 gcloud config set project ${IGRESS_PROJECT}
 
 gcloud container clusters get-credentials ${EGRESS_CLUSTER} --region ${EGRESS_LOCATION} --project ${EGRESS_PROJECT}
+## In case of Anthos on AWS use the following command to get credentials:
+#anthos-gke aws clusters get-credentials ${EGRESS_CLUSTER}
+
+## In case of Anthos GKE On-Prem, use the following command to acess the clsuter:
+#export KUBECONFIG=${EGRESS_CLUSTER_KUBECONFIG}
