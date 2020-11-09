04ef087191b7a2d7655f06eb388aebfd189677ab
jsolarz <jsolarz@google.com>
2020-11-09 23:22:43 +0100

    Use repo (git clone) in key creation

--- create-keys.sh
+++ create-keys.sh
@@ -4,10 +4,14 @@ INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonp
 
 rm -rf certs
 mkdir certs
+git clone git@github.com:nicholasjackson/mtls-go-example.git
 cd mtls-go-example
 # delete old certs
 rm -rf 1_root 2_intermediate 3_application 4_client
-yes | ./generate.sh $INGRESS_HOST.nip.io mysupersecurepassword
+echo "Please enter a password for your security keys: "
+read SECURE_PASSWORD
+
+yes | ./generate.sh $INGRESS_HOST.nip.io $SECURE_PASSWORD
 mv 1_root ../certs
 mv 2_intermediate ../certs
 mv 3_application ../certs
