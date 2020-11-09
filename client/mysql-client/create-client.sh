a23e368f9920bf98f40346173ca65b8dc02c1c68
jsolarz <jsolarz@google.com>
2020-11-09 23:22:14 +0100

    Split Istio definition in separate files

--- client/mysql-client/create-client.sh
+++ client/mysql-client/create-client.sh
@@ -8,6 +8,8 @@ CERTS_ROOT="../../httpbin-certs"
 # url of the external service
 SERVICE_URL="httpbin-mutual-tls.jeremysolarz.app"
 
+kubectl apply -f service-entry.yaml
+
 kubectl create -n istio-system secret tls httpbin-client-certs \
   --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
   --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem
@@ -20,4 +22,10 @@ sleep 15
 kubectl exec -it -n istio-system $(kubectl -n istio-system get pods -l istio=egressgateway -o jsonpath='{.items[0].metadata.name}') \
   -- ls -al /etc/istio/httpbin-client-certs /etc/istio/httpbin-ca-certs
 
-kubectl apply -f se-mysql.yaml
+# setup routing for the mysql service inside the mesh
+kubectl apply -f gateway-destinationrule-to-egressgateway.yaml
+
+# now activate the virtualservice that does the routing from the pod to the egress-gateway and from the
+# egress gateway outside, additionally tell the egress gateway to use the certificates mounted before for
+# the outbound traffic (via the destination rule)
+kubectl apply -f virtualservice-destinationrule-from-egressgateway.yaml
