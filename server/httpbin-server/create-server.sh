f09686112eab60a8234cb66e44b485e8e64fcb7d
jsolarz <jsolarz@google.com>
2020-11-23 23:36:10 +0100

    Add more sleep to allow for proper server startup Rearrange kubectl commands after variables

--- server/httpbin-server/create-server.sh
+++ server/httpbin-server/create-server.sh
@@ -8,10 +8,10 @@ CERTS_ROOT="../../certs"
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
 
-kubectl apply -f httpbin.yaml
-
 # url of the external service
 SERVICE_URL="$INGRESS_HOST.nip.io"
+
+kubectl apply -f httpbin.yaml
 
 kubectl create -n istio-system secret generic httpbin-credential \
 --from-file=tls.key=$CERTS_ROOT/3_application/private/${SERVICE_URL}.key.pem \
@@ -28,7 +28,7 @@ kubectl apply -f virtual-service.yaml
 kubectl apply -f gateway-mutual.yaml
 
 
-sleep 5
+sleep 15
 # jsolarz-sanbox
 curl -v -HHost:${SERVICE_URL} --resolve \
   "${SERVICE_URL}:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
@@ -37,6 +37,7 @@ curl -v -HHost:${SERVICE_URL} --resolve \
   --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
   "https://${SERVICE_URL}:$SECURE_INGRESS_PORT/status/418"
 
+sleep 15
 # external
 curl -v --cacert $CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem \
   --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem \
