a6bff6f52faf84a3d824d754e5ad2db641e2ac3d
jsolarz <jsolarz@google.com>
2020-12-15 11:49:04 +0100

    Add test if mtls-go-example repo has already been cloned. Add test that password has been provided (empty password for certs does not work).

--- create-keys.sh
+++ create-keys.sh
@@ -4,14 +4,20 @@ INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonp
 
 rm -rf certs
 mkdir certs
-git clone git@github.com:nicholasjackson/mtls-go-example.git
+[ ! -d mtls-go-example ]  && git clone git@github.com:nicholasjackson/mtls-go-example.git
 cd mtls-go-example
 # delete old certs
 rm -rf 1_root 2_intermediate 3_application 4_client
 echo "Please enter a password for your security keys: "
 read SECURE_PASSWORD
 
+if [[ -z "$SECURE_PASSWORD" ]]; then
+   printf '%s\n' "Please provide a password."
+   exit 1
+fi
+
 yes | ./generate.sh $INGRESS_HOST.nip.io $SECURE_PASSWORD
+
 mv 1_root ../certs
 mv 2_intermediate ../certs
 mv 3_application ../certs
