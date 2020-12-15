0277bf32bbc6cf6e318cf7eee7ac61489898f05d
jsolarz <jsolarz@google.com>
2020-12-15 15:39:07 +0100

    Move helper to get sleeper proxy logs

--- client/get-logs-from-sleep-proxy.sh
+++
@@ -1,3 +0,0 @@
-./set-project-and-cluster-client.sh
-
-kubectl logs -l app=sleep -c istio-proxy -f
---
+++ client/httpbin-client/get-logs-from-sleep-proxy.sh
@@ -0,0 +1,5 @@
+. ../../env-vars
+
+../set-project-and-cluster-client.sh
+
+kubectl logs -l app=sleep -c istio-proxy -f
