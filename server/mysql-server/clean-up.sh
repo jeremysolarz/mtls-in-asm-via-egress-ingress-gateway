a047695dacdd8d4965eee499bae192a770377d7a
jsolarz <jsolarz@google.com>
2020-12-16 17:40:03 +0100

    Add clean-up.sh for mysql-server side.

---
+++ server/mysql-server/clean-up.sh
@@ -0,0 +1,15 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. $DIR/../../env-vars
+
+$DIR/../set-project-and-cluster-server.sh
+
+kubectl delete --ignore-not-found=true -n istio-system secret mysql-credential
+
+# todo remove ingress patch
+kubectl delete --ignore-not-found=true gateway mysql-gateway
+kubectl delete --ignore-not-found=true virtualservice mysql-virtual-service
+
+kubectl delete --ignore-not-found=true service mysql
+kubectl delete --ignore-not-found=true deploy mysql
+kubectl delete --ignore-not-found=true pvc mysql-pv-claim
