8e207e0203d15e6d114e8fabd96e1fd3c6f11853
jsolarz <jsolarz@google.com>
2020-12-17 03:32:01 +0100

    Add dynamic SERVICE_URL to create-client.sh Hardcoded service url in Istio YAMLs needs to be updated. Update clean-up.sh

--- client/mysql-client/clean-up.sh
+++ client/mysql-client/clean-up.sh
@@ -1,13 +1,19 @@
-../set-project-and-cluster-client.mesh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-kubectl delete --ignore-not-found=true -f se-mysql.yaml
-kubectl delete --ignore-not-found=true secret httpbin-client-certs httpbin-ca-certs
-kubectl delete --ignore-not-found=true secret httpbin-client-certs httpbin-ca-certs -n istio-system
+$DIR/../set-project-and-cluster-client.sh
+
+# todo cleanup cleanup.sh :D
+kubectl delete --ignore-not-found=true secret mysql-client-certs mysql-ca-certs
+kubectl delete --ignore-not-found=true secret mysql-client-certs mysql-ca-certs -n istio-system
+
+kubectl delete -f gateway-destinationrule-to-egressgateway.yaml
+kubectl delete -f virtualservice-destinationrule-from-egressgateway.yaml
+kubectl delete -f service-entry.yaml
 
 # reset ASM
-istioctl install \
-  -f ../../../istio-1.6.8-asm.9/asm/cluster/istio-operator.yaml \
--f ../features.yaml
-
-kubectl get deploy -n istio-system
-kubectl get ns
+#istioctl install \
+#  -f ../../../istio-1.6.8-asm.9/asm/cluster/istio-operator.yaml \
+#-f ../features.yaml
+#
+#kubectl get deploy -n istio-system
+#kubectl get ns
--- client/mysql-client/create-client.sh
+++ client/mysql-client/create-client.sh
@@ -1,26 +1,36 @@
-../set-project-and-cluster-client.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-./clean-up.sh
+. $DIR/../../env-vars
+
+$DIR/../../server/set-project-and-cluster-server.sh
+
+# !!! WARNING - DO NOT MOVE !!!
+# get Ingress IP from server side
+export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+$DIR/../set-project-and-cluster-client.sh
+
+$DIR/./clean-up.sh
 
 # folder where certificates are stored created by mtls-go-example
-CERTS_ROOT="../../httpbin-certs"
+CERTS_ROOT="$DIR/../../certs"
 
 # url of the external service
 SERVICE_URL="httpbin-mutual-tls.jeremysolarz.app"
 
 kubectl apply -f service-entry.yaml
 
-kubectl create -n istio-system secret tls httpbin-client-certs \
+kubectl create -n istio-system secret tls mysql-client-certs \
   --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
   --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem
 
-kubectl create -n istio-system secret generic httpbin-ca-certs --from-file=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
+kubectl create -n istio-system secret generic mysql-ca-certs --from-file=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
 
-kubectl -n istio-system patch --type=json deploy istio-egressgateway -p "$(cat gateway-patch.json)"
+# kubectl -n istio-system patch --type=json deploy istio-egressgateway -p "$(cat gateway-patch.json)"
 
 sleep 15
 kubectl exec -it -n istio-system $(kubectl -n istio-system get pods -l istio=egressgateway -o jsonpath='{.items[0].metadata.name}') \
-  -- ls -al /etc/istio/httpbin-client-certs /etc/istio/httpbin-ca-certs
+  -- ls -al /etc/istio/mysql-client-certs /etc/istio/mysql-ca-certs
 
 # setup routing for the mysql service inside the mesh
 kubectl apply -f gateway-destinationrule-to-egressgateway.yaml
