3d433a31c958edaf9e980f1c39281a1af714d6f3
vhamburger <vhamburger@google.com>
2020-12-17 14:11:46 +0100

    kops tf install script fixes

--- kops/cluster/b_create-kops-cluster.sh
+++ kops/cluster/b_create-kops-cluster.sh
@@ -10,7 +10,7 @@ export KOPS_FEATURE_FLAGS=AlphaAllowGCE
 export KOPS_STATE_STORE=$PROJECT-kops-clusters
 #source ../env.txt
 echo "Installing and running kops clsuter now"
-kops create cluster mtls.k8s.local --zones $GCPZONE --state ${KOPS_STATE_STORE}/ --project=${PROJECT}
+kops create cluster mtls.k8s.local --zones $GCPZONE --state $KOPS_STATE_STORE/ --project=$PROJECT
 #echo "Cluster object has been created:"
 #kops get cluster --state ${KOPS_STATE_STORE}
 echo "Starting cluster instances now"
