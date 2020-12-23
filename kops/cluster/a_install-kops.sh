853ea0be90d6da1f5db27b30ff68fb1b29f648aa
vhamburger <vhamburger@google.com>
2020-12-23 15:24:31 +0100

    Finish kops installation procedure / correct cluster size and kubeconfig export

--- kops/cluster/a_install-kops.sh
+++ kops/cluster/a_install-kops.sh
@@ -12,14 +12,14 @@ Darwin*)
     echo "Installing kOps for MacOs now"
     curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-darwin-amd64
     chmod +x ./kops
-    sudo mv ./kops /usr/local/bin/
-    echo "Done dowwnloading"
+    mv ./kops /tmp/
+    echo "Done downloading"
     ;;
 Linux*)
     echo "Insallin kOps for Linux now"
     curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
     chmod +x ./kops
-    sudo mv ./kops /usr/local/bin/
+    sudo mv ./kops /tmp/
     echo "Done downloading kops"
     ;;
 *) 
@@ -28,9 +28,6 @@ Linux*)
     ;;
 esac
 
-echo "Setting envs file now"
-PROJECT=$(gcloud config get-value project)
-echo ""
 echo "Generating kops bucket"
 gsutil mb gs://$PROJECT-kops-clusters
 
--- kops/cluster/b_create-kops-cluster.sh
+++ kops/cluster/b_create-kops-cluster.sh
@@ -10,7 +10,7 @@ export KOPS_FEATURE_FLAGS='${kops-gce}'
 
 #source ../env-vars
 echo "Installing and running kops cluster now"
-kops create cluster mtls.k8s.local --zones $GCPZONE --state $PROJECT-kops-clusters/ --project=$PROJECT
+kops create cluster mtls.k8s.local --zones $GCPZONE --state "gs://$PROJECT-kops-clusters/"/ --project=$PROJECT --node-count=4
 #echo "Cluster object has been created:"
 echo "Starting cluster instances now"
-kops update cluster mtls.k8s.local --yes
+kops update cluster mtls.k8s.local --yes --state "gs://$PROJECT-kops-clusters"/
