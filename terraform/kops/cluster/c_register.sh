45383e30dd2c8dee64923eea4758eeb4e55d288d
vhamburger <vhamburger@google.com>
2021-01-05 15:36:29 +0100

    Moved kops folder iniside of terraform folder. Kubeconfig will also be created now in the terraform folder for ease of access.

--- terraform/kops/cluster/c_register.sh
+++ terraform/kops/cluster/c_register.sh
@@ -2,6 +2,7 @@
 #source env-vars
 ## TF vars
 export PROJECT='${project}'
+export LOCATION='${location}'
 ##
 
 echo -e "This will enable the Anthos APIs in this project $PROJECT!\n 
@@ -28,7 +29,7 @@ gcloud iam service-accounts keys create gkehub.json \
   --iam-account=client-cluster-gke-hub-sa@$PROJECT.iam.gserviceaccount.com \
   --project=$PROJECT
 echo "Storing kOps kubeconfig in mtls-kubeconfig"
-kops export kubecfg mtls.k8s.local --kubeconfig /tmp/mtls-kubeconfig --state "gs://$PROJECT-kops-clusters/"/
+kops export kubecfg mtls.k8s.local --kubeconfig $LOCATION/server-kubeconfig --state "gs://$PROJECT-kops-clusters/"/
 echo "Registering kOps cluster into Anthos in project: $PROJECT"
 gcloud container hub memberships register server-cluster \
             --context=mtls.k8s.local \
@@ -37,11 +38,11 @@ gcloud container hub memberships register server-cluster \
             --project=$PROJECT \
             --quiet
 echo "Creating login token for CloudConsole (admin account!)"
-kubectl --kubeconfig /tmp/mtls-kubeconfig create serviceaccount -n kube-system admin-user
-kubectl --kubeconfig /tmp/mtls-kubeconfig create clusterrolebinding admin-user-binding \
+kubectl --kubeconfig $LOCATION/server-kubeconfig create serviceaccount -n kube-system admin-user
+kubectl --kubeconfig $LOCATION/server-kubeconfig create clusterrolebinding admin-user-binding \
   --clusterrole cluster-admin --serviceaccount kube-system:admin-user
-SECRET_NAME=$(kubectl --kubeconfig /tmp/mtls-kubeconfig get serviceaccount -n kube-system admin-user \
+SECRET_NAME=$(kubectl --kubeconfig $LOCATION/server-kubeconfig get serviceaccount -n kube-system admin-user \
   -o jsonpath='{$.secrets[0].name}')
 echo "Copy this token and use it to login to your cluster in cloud console"
-echo $(kubectl --kubeconfig /tmp/mtls-kubeconfig get secret -n kube-system $SECRET_NAME -o jsonpath='{$.data.token}' \
+echo $(kubectl --kubeconfig $LOCATION/server-kubeconfig get secret -n kube-system $SECRET_NAME -o jsonpath='{$.data.token}' \
   | base64 -d | sed $'s/$/\\\n/g') >> kops-ksa.token
