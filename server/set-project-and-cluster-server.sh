dff1eca7faa3b11af306a816efd73095efc64570
jsolarz <jsolarz@google.com>
2021-01-06 22:24:35 +0100

    Rename DIR variable to SERVER_DIR to not override other DIR variables

--- server/set-project-and-cluster-server.sh
+++ server/set-project-and-cluster-server.sh
@@ -1,5 +1,6 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#
+SERVER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 gcloud config set project ${PROJECT_ID}
 
-export KUBECONFIG="${DIR}/../terraform/server-kubeconfig"
+export KUBECONFIG="${SERVER_DIR}/../terraform/server-kubeconfig"
