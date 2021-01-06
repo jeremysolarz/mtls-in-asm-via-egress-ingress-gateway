36c4008f7898d7d0a9d053fdc0cfd710516d4d95
jsolarz <jsolarz@google.com>
2021-01-06 13:33:08 +0100

    Use PROJECT_ID (from Tutorial guide), ZONE, client-cluster and server-kubeconfig (from Terraform) as client / server Kubeconfig endpoints

--- server/set-project-and-cluster-server.sh
+++ server/set-project-and-cluster-server.sh
@@ -1,13 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-. $DIR/../env-vars
+gcloud config set project ${PROJECT_ID}
 
-gcloud config set project ${INGRESS_PROJECT}
-
-gcloud container clusters get-credentials ${INGRESS_CLUSTER} --region ${INGRESS_LOCATION} --project ${INGRESS_PROJECT}
-
-## In case of Anthos on AWS use the following command to get credentials:
-#anthos-gke aws clusters get-credentials ${EGRESS_CLUSTER}
-
-## In case of Anthos GKE On-Prem, use the following command to acess the clsuter:
-#export KUBECONFIG=${EGRESS_CLUSTER_KUBECONFIG}
+export KUBECONFIG="${DIR}/../terraform/server-kubeconfig"
