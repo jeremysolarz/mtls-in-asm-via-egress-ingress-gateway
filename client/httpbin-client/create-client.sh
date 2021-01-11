975b525443cab526313e49d1ce069ca224f3363f
jsolarz <jsolarz@google.com>
2021-01-11 19:28:32 +0100

    Change secret file-mount to SDS

--- client/httpbin-client/create-client.sh
+++ client/httpbin-client/create-client.sh
@@ -50,26 +50,25 @@ kubectl exec -it $SOURCE_POD -c sleep -- curl -v \
   --cert /etc/httpbin-client-certs/tls.crt \
   --key /etc/httpbin-client-certs/tls.key https://${SERVICE_URL}/status/418
 
-# TODO replace file-mount with SDS
-# kubectl create secret -n istio-system generic client-credential \
-#  --from-file=tls.key=$CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
-#  --from-file=tls.crt=$CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem \
-#  --from-file=ca.crt=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
+kubectl create secret -n istio-system generic client-credential \
+  --from-file=tls.key=$CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
+  --from-file=tls.crt=$CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem \
+  --from-file=ca.crt=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
 
 
 # create the secrets that hold the certificates for the egress proxy
-kubectl create -n istio-system secret tls httpbin-client-certs \
-  --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
-  --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem
-kubectl create -n istio-system secret generic httpbin-ca-certs \
-  --from-file=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
+# kubectl create -n istio-system secret tls httpbin-client-certs \
+#  --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
+#  --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem
+# kubectl create -n istio-system secret generic httpbin-ca-certs \
+#  --from-file=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
 
 # patch the egress gateway to mount the secret (aka certificates)
-kubectl -n istio-system patch --type=json deploy istio-egressgateway -p "$(cat gateway-patch.json)"
+# kubectl -n istio-system patch --type=json deploy istio-egressgateway -p "$(cat gateway-patch.json)"
 
 sleep 30
-kubectl exec -it -n istio-system $(kubectl -n istio-system get pods -l istio=egressgateway -o jsonpath='{.items[0].metadata.name}') \
-  -- ls -al /etc/istio/httpbin-client-certs /etc/istio/httpbin-ca-certs
+# kubectl exec -it -n istio-system $(kubectl -n istio-system get pods -l istio=egressgateway -o jsonpath='{.items[0].metadata.name}') \
+#  -- ls -al /etc/istio/httpbin-client-certs /etc/istio/httpbin-ca-certs
 
 # setup routing for the mysql service inside the mesh
 kubectl apply -f gateway-destinationrule-to-egressgateway.yaml
