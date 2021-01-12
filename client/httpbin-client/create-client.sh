8c34292d47bd50eb5f38b5af42870ded080de17d
jsolarz <jsolarz@google.com>
2021-01-12 16:46:35 +0100

    Remove old code for file-mount

--- client/httpbin-client/create-client.sh
+++ client/httpbin-client/create-client.sh
@@ -55,20 +55,7 @@ kubectl create secret -n istio-system generic client-credential \
   --from-file=tls.crt=$CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem \
   --from-file=ca.crt=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
 
-
-# create the secrets that hold the certificates for the egress proxy
-# kubectl create -n istio-system secret tls httpbin-client-certs \
-#  --key $CERTS_ROOT/4_client/private/${SERVICE_URL}.key.pem \
-#  --cert $CERTS_ROOT/4_client/certs/${SERVICE_URL}.cert.pem
-# kubectl create -n istio-system secret generic httpbin-ca-certs \
-#  --from-file=$CERTS_ROOT/2_intermediate/certs/ca-chain.cert.pem
-
-# patch the egress gateway to mount the secret (aka certificates)
-# kubectl -n istio-system patch --type=json deploy istio-egressgateway -p "$(cat gateway-patch.json)"
-
 sleep 30
-# kubectl exec -it -n istio-system $(kubectl -n istio-system get pods -l istio=egressgateway -o jsonpath='{.items[0].metadata.name}') \
-#  -- ls -al /etc/istio/httpbin-client-certs /etc/istio/httpbin-ca-certs
 
 # setup routing for the mysql service inside the mesh
 kubectl apply -f gateway-destinationrule-to-egressgateway.yaml
